d6e22cfa2a0ff182473d885869d43cba2c077a13
Sindhu Muralidhar <sindhu.comp@gmail.com>
2019-03-10 16:45:46 -0700

    initial3

---
+++ PatientInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PatientInfo
+/// <summary>
+/// Information on the Patient details
+/// </summary>
+{
+   
+    class Information
+        
+    {
+        #region Properties
+
+           public char PatientName { get; set; }
+        //Name of the Patient
+           public string DateOfBirth { get; set; }
+        //Patient's Date of birth
+           public int ContactNumber { get; set; }
+        //Contact number of the Patient
+           public decimal Weight { get; set; }
+        //Current weight of the Patient
+           public bool InsuranceAvailability{ get; set; }
+        // Insurance Availabilty for the given Patient - Yes or No.
+
+        #endregion
+
+
+    }
+    #region Methods
+    /// <summary>
+    /// Enters the Patient's name
+    /// </summary>
+    /// <param name="PatientName"></param> Patient's name entry
+    
+{
+    
+   
+}
+}
